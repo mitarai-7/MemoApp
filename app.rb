406215714a2dd70c52c114ffc6fec6fea1015c65
mitarai-7 <eveandartys+github@gmail.com>
2021-05-08 20:58:13 +0900

    各機能の実装完了

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ require 'json'
 
 set :public_folder, 'public'
 
-get '/' do
-  'hello'
-end
-
 get '/list' do
   datastore = 'data/memo.json'
   @memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) } if File.readable?(datastore)
@@ -17,21 +13,19 @@ get '/list' do
 end
 
 get '/new' do
-  erb :edit
+  erb :new
 end
 
 post '/new' do
-  # title = params[:title]
-  # text = params[:text]
-
   datastore = 'data/memo.json'
   if File.writable?(datastore)
     memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) }
-    # params[:id] = 
+    index = memo_list.size - 1
+    params[:id] = ((memo_list[index])['id'].to_i + 1).to_s
     memo_list.push(params)
     File.open(datastore, 'w') { |io| io.print JSON.generate(memo_list) }
   end
-  erb :edit
+  redirect to('/list')
 end
 
 get '/memo/:id' do
@@ -39,12 +33,43 @@ get '/memo/:id' do
   if File.readable?(datastore)
     memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) }
     memo = memo_list.find { |x| x['id'] == params[:id] }
+    @id = memo['id']
     @title = memo['title']
     @text = memo['text']
   end
   erb :detail
 end
 
-patch '/memo/:id' do |id|
+get '/edit/:id' do
+  datastore = 'data/memo.json'
+  if File.readable?(datastore)
+    memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) }
+    memo = memo_list.find { |x| x['id'] == params[:id] }
+    @id = memo['id']
+    @title = memo['title']
+    @text = memo['text']
+  end
+  erb :edit
+end
 
+patch '/edit/:id' do
+  datastore = 'data/memo.json'
+  if File.writable?(datastore)
+    memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) }
+    index = memo_list.find_index { |x| x['id'] == params[:id] }
+    (memo_list[index])['title'] = params[:title]
+    (memo_list[index])['text'] = params[:text]
+    File.open(datastore, 'w') { |io| io.print JSON.generate(memo_list) }
+  end
+  200
+end
+
+delete '/memo/:id' do
+  datastore = 'data/memo.json'
+  if File.writable?(datastore)
+    memo_list = File.open(datastore, 'r') { |io| JSON.parse(io.read) }
+    memo_list.delete_if { |x| x['id'] == params[:id] }
+    File.open(datastore, 'w') { |io| io.print JSON.generate(memo_list) }
+  end
+  200
 end
