6cf53935ad6fd23a288e411698216ed3cc6bcaf8
mitarai-7 <eveandartys+github@gmail.com>
2021-06-06 10:00:57 +0900

    読み込みのメソッドを作成

--- lib/memo_repository.rb
+++ lib/memo_repository.rb
@@ -14,6 +14,23 @@ class MemoRepository
   );
   SQL
 
+  SQL_READ = <<-SQL
+  SELECT
+    id,
+    title,
+    text
+  FROM memos
+  WHERE id = $1;
+  SQL
+
+  SQL_READALL = <<-SQL
+  SELECT
+    id,
+    title,
+    text
+  FROM memos;
+  SQL
+
   def initialize
     @conn = PG::Connection.new(dbname: 'memo')
     # sql = <<-SQL
@@ -25,13 +42,34 @@ class MemoRepository
     # SQL
     # @conn.exec(sql)
     @conn.prepare('create', SQL_CREATE)
+    @conn.prepare('read', SQL_READ)
+    @conn.prepare('readall', SQL_READALL)
   end
 
   def create
     res = @conn.exec_prepared('create', %w[title text])
     p res
   end
+
+  def read(id = nil)
+    res = []
+    if id.nil?
+      @conn.exec_prepared('readall') do |memos|
+        memos.each do |memo|
+          res << { id: memo.values[0], title: memo.values[1], text: memo.values[2] }
+        end
+      end
+    else
+      @conn.exec_prepared('read', [id]) do |memos|
+        memos.each do |memo|
+          res << { id: memo.values[0], title: memo.values[1], text: memo.values[2] }
+        end
+      end
+    end
+    res
+  end
 end
 
 mr = MemoRepository.new
-mr.create
+# mr.create
+mr.read(1)
