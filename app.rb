164de592a21d2e30d5e0757e5426da3b9778a2e8
mitarai-7 <eveandartys+github@gmail.com>
2021-06-06 14:23:22 +0900

    他のAPIをDBへ移行した

--- app.rb
+++ app.rb
@@ -25,59 +25,32 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  memo = { memo_title: params['memo_title'], memo_text: params['memo_text'] }
-  if File.writable?(DATASTORE)
-    memo_list = File.open(DATASTORE, 'r') { |io| JSON.parse(io.read) }
-    if memo_list.empty?
-      memo[:id] = '1'
-    else
-      index = memo_list.size - 1
-      memo[:id] = ((memo_list[index])['id'].to_i + 1).to_s
-    end
-    memo_list.push(memo)
-    File.open(DATASTORE, 'w') { |io| io.print JSON.generate(memo_list) }
-  end
+  mr.create(params['memo_title'], params['memo_text'])
   redirect to('/memos')
 end
 
 get '/memos/:id' do
-  if File.readable?(DATASTORE)
-    memo_list = File.open(DATASTORE, 'r') { |io| JSON.parse(io.read) }
-    memo = memo_list.find { |x| x['id'] == params[:id] }
-    @id = memo['id']
-    @title = memo['memo_title']
-    @text = memo['memo_text'].gsub(/\n/, '<br>')
-  end
+  memo = mr.read(params[:id]).first
+  @id = memo[:id]
+  @title = memo[:memo_title]
+  @text = memo[:memo_text].gsub(/\n/, '<br>')
   erb :detail
 end
 
 delete '/memos/:id' do
-  if File.writable?(DATASTORE)
-    memo_list = File.open(DATASTORE, 'r') { |io| JSON.parse(io.read) }
-    memo_list.delete_if { |x| x['id'] == params[:id] }
-    File.open(DATASTORE, 'w') { |io| io.print JSON.generate(memo_list) }
-  end
+  mr.delete(params[:id])
   redirect to('/memos')
 end
 
 get '/memos/:id/edit' do
-  if File.readable?(DATASTORE)
-    memo_list = File.open(DATASTORE, 'r') { |io| JSON.parse(io.read) }
-    memo = memo_list.find { |x| x['id'] == params[:id] }
-    @id = memo['id']
-    @title = memo['memo_title']
-    @text = memo['memo_text']
-  end
+  memo = mr.read(params[:id]).first
+  @id = memo[:id]
+  @title = memo[:memo_title]
+  @text = memo[:memo_text].gsub(/\n/, '<br>')
   erb :edit
 end
 
 patch '/memos/:id' do
-  if File.writable?(DATASTORE)
-    memo_list = File.open(DATASTORE, 'r') { |io| JSON.parse(io.read) }
-    index = memo_list.find_index { |x| x['id'] == params[:id] }
-    (memo_list[index])['memo_title'] = params['memo_title']
-    (memo_list[index])['memo_text'] = params['memo_text']
-    File.open(DATASTORE, 'w') { |io| io.print JSON.generate(memo_list) }
-  end
+  mr.update(params['id'], params['memo_title'], params['memo_text'])
   redirect to('/memos')
 end
